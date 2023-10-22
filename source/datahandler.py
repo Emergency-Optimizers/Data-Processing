3beae4bc385c7562fde743fca334eef06daedda5
Sindre Eiklid <sindre0830@gmail.com>
2023-10-22 18:03:53 +0200

    Add `_process_data` method

--- source/datahandler.py
+++ source/datahandler.py
@@ -38,12 +38,10 @@ class DataPreprocessor:
     def _clean_data(self) -> None:
         """Clean the raw data and cache it."""
         os.makedirs(os.path.dirname(self._clean_incidents_data_path), exist_ok=True)
-        pass
 
     def _process_data(self) -> None:
         """Process the clean data and cache it."""
         os.makedirs(os.path.dirname(self._processed_incidents_data_path), exist_ok=True)
-        pass
 
 
 class DataPreprocessorOUS(DataPreprocessor):
@@ -103,3 +101,85 @@ class DataPreprocessorOUS(DataPreprocessor):
 
     def _process_data(self) -> None:
         super()._process_data()
+        progress_bar = tqdm(desc="Processing dataset", total=2)
+
+        if not os.path.exists(self._processed_incidents_data_path):
+            # load cleaned dataset
+            df_incidents_clean = pd.read_csv(self._clean_incidents_data_path, low_memory=False)
+            # create columns
+            column_data_types = {
+                "id": "int32",
+                "synthetic": "bool",
+                "triage_impression_during_call": "str",
+                "time_call_received": "str",
+                "time_dispatch": "str",
+                "time_arrival_scene": "str",
+                "time_departure_scene": "str",
+                "time_arrival_hospital": "str",
+                "time_available": "str",
+                "longitude": "float32",
+                "latitude": "float32"
+            }
+            # create rows
+            row_data = {
+                "id": [],
+                "synthetic": [],
+                "triage_impression_during_call": [],
+                "time_call_received": [],
+                "time_dispatch": [],
+                "time_arrival_scene": [],
+                "time_departure_scene": [],
+                "time_arrival_hospital": [],
+                "time_available": [],
+                "longitude": [],
+                "latitude": []
+            }
+            # iterate over cleaned dataset
+            for _, row in df_incidents_clean.iterrows():
+                row_data["id"].append(row["id"])
+                row_data["synthetic"].append(False)
+                row_data["triage_impression_during_call"].append(row["hastegrad"])
+                row_data["time_call_received"].append(row["tidspunkt"] if not pd.isna(row["tidspunkt"]) else row["tiltak_opprettet"])
+                row_data["time_dispatch"].append(row["rykker_ut"] if not pd.isna(row["rykker_ut"]) else row["varslet"])
+                row_data["time_arrival_scene"].append(row["ank_hentested"])
+                row_data["time_departure_scene"].append(row["avg_hentested"])
+                row_data["time_arrival_hospital"].append(row["ank_levsted"])
+                row_data["time_available"].append(row["ledig"])
+                row_data["longitude"].append(row["xcoor"])
+                row_data["latitude"].append(row["ycoor"])
+            # convert to dataframe and save to disk
+            df_incidents = pd.DataFrame(row_data)
+            for column, dtype in column_data_types.items():
+                df_incidents[column] = df_incidents[column].astype(dtype)
+            df_incidents.to_csv(self._processed_incidents_data_path, index=False)
+        progress_bar.update(1)
+
+        if not os.path.exists(self._processed_depots_data_path):
+            # load cleaned dataset
+            df_depots_clean = pd.read_csv(self._clean_depots_data_path)
+            # create columns
+            column_data_types = {
+                "id": "int32",
+                "static": "bool",
+                "longitude": "float32",
+                "latitude": "float32"
+            }
+            # create rows
+            row_data = {
+                "id": [],
+                "static": [],
+                "longitude": [],
+                "latitude": []
+            }
+            # iterate over cleaned dataset
+            for _, row in df_depots_clean.iterrows():
+                row_data["id"].append(row["id"])
+                row_data["static"].append(True if row["type"] == "stasjon" else False)
+                row_data["longitude"].append(row["longitude"])
+                row_data["latitude"].append(row["latitude"])
+            # convert to dataframe and save to disk
+            df_depots = pd.DataFrame(row_data)
+            for column, dtype in column_data_types.items():
+                df_depots[column] = df_depots[column].astype(dtype)
+            df_depots.to_csv(self._processed_depots_data_path, index=False)
+        progress_bar.update(1)
