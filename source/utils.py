f44557b2174bdecab440eee7616adcf217be3d3d
Torjus Kallekleiv <torjus.kallekleiv@gmail.com>
2023-10-24 11:23:55 +0200

    Added UTM conversion functions

--- source/utils.py
+++ source/utils.py
@@ -2,6 +2,7 @@ import constants
 
 import os
 import pandas as pd
+import pyproj
 
 
 def get_raw_incidents_path(dataset_id: str) -> str:
@@ -34,3 +35,19 @@ def get_enhanced_incidents_path(dataset_id: str) -> str:
 
 def get_enhanced_depots_path(dataset_id: str) -> str:
     return os.path.join(constants.PROJECT_DIRECTORY_PATH, "data", "enhanced", dataset_id, "depots.csv")
+
+
+def latlon_to_utm(lat, lon, false_easting=0):
+    wgs84 = pyproj.Proj(proj="latlong", datum="WGS84")
+    utm33 = pyproj.Proj(proj="utm", zone=33, datum="WGS84")
+    utm_x, utm_y = pyproj.transform(wgs84, utm33, lon, lat)
+    utm_x += false_easting  # Add false easting if specified
+    return utm_x, utm_y
+
+
+def utm_to_latlon(utm_x, utm_y, false_easting=0):
+    wgs84 = pyproj.Proj(proj="latlong", datum="WGS84")
+    utm33 = pyproj.Proj(proj="utm", zone=33, datum="WGS84")
+    utm_x -= false_easting  # Remove false easting if specified
+    lon, lat = pyproj.transform(utm33, wgs84, utm_x, utm_y)
+    return lat, lon
