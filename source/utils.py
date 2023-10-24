898b1975c143f1626603ba47ee2d2ddeeaebd53a
Torjus Kallekleiv <torjus.kallekleiv@gmail.com>
2023-10-24 11:27:14 +0200

    Added zone option for UTM conversion function

--- source/utils.py
+++ source/utils.py
@@ -37,17 +37,25 @@ def get_enhanced_depots_path(dataset_id: str) -> str:
     return os.path.join(constants.PROJECT_DIRECTORY_PATH, "data", "enhanced", dataset_id, "depots.csv")
 
 
-def latlon_to_utm(lat, lon, false_easting=0):
+def latlon_to_utm(lat, lon, false_easting=0, zone=33):
+    """
+    zone: int, optional
+        UTM zone for the conversion. Default is 33 for Norway, use 10 for Marin County, US.
+    """
     wgs84 = pyproj.Proj(proj="latlong", datum="WGS84")
-    utm33 = pyproj.Proj(proj="utm", zone=33, datum="WGS84")
+    utm33 = pyproj.Proj(proj="utm", zone=zone, datum="WGS84")
     utm_x, utm_y = pyproj.transform(wgs84, utm33, lon, lat)
     utm_x += false_easting  # Add false easting if specified
     return utm_x, utm_y
 
 
-def utm_to_latlon(utm_x, utm_y, false_easting=0):
+def utm_to_latlon(utm_x, utm_y, false_easting=0, zone=33):
+    """
+    zone: int, optional
+        UTM zone for the conversion. Default is 33 for Norway, use 10 for Marin County, US.
+    """
     wgs84 = pyproj.Proj(proj="latlong", datum="WGS84")
-    utm33 = pyproj.Proj(proj="utm", zone=33, datum="WGS84")
+    utm33 = pyproj.Proj(proj="utm", zone=zone, datum="WGS84")
     utm_x -= false_easting  # Remove false easting if specified
     lon, lat = pyproj.transform(utm33, wgs84, utm_x, utm_y)
     return lat, lon
