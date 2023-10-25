ba7f67a00a20197604776e94127dc3e552f49f6e
Torjus Kallekleiv <torjus.kallekleiv@gmail.com>
2023-10-25 11:20:38 +0200

    Added COLOR_MAPPING_DEEP

--- source/constants.py
+++ source/constants.py
@@ -1,11 +1,13 @@
 import os
-
+import seaborn as sns
 
 PROJECT_DIRECTORY_PATH = os.path.dirname(os.path.dirname(__file__))
 
-COLOR_MAPPING = {
-    'A': 'red',
-    'H': 'blue',
-    'V1': 'green',
-    'V2': 'purple'
+deep_palette = sns.color_palette("deep")
+
+COLOR_MAPPING_DEEP = {
+    'A': deep_palette[3],
+    'H': deep_palette[1],
+    'V1': deep_palette[8],
+    'V2': deep_palette[2]
 }
