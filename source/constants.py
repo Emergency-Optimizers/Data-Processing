585fe7eb08a93c873208fdfc5a54ca696e0011af
sindre0830 <sindre0830@gmail.com>
2023-10-25 12:46:21 +0200

    Update aborted missions notebook

--- source/constants.py
+++ source/constants.py
@@ -1,5 +1,7 @@
 import os
 import seaborn as sns
+import matplotlib.pyplot as plt
+
 
 PROJECT_DIRECTORY_PATH = os.path.dirname(os.path.dirname(__file__))
 
@@ -11,3 +13,4 @@ COLOR_MAPPING_DEEP = {
     'V1': deep_palette[8],
     'V2': deep_palette[2]
 }
+COLOR_MAPPING_LIGHT = [plt.cm.colors.to_rgba(color, alpha=0.65) for color in COLOR_MAPPING_DEEP.values()]
