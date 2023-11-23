5e035a507f7cedfcfbde74e5764651ad8d492277
Torjus Kallekleiv <torjus.kallekleiv@gmail.com>
2023-11-23 13:48:56 +0100

    Added new colors

--- source/constants.py
+++ source/constants.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 PROJECT_DIRECTORY_PATH = os.path.dirname(os.path.dirname(__file__))
 
+normal_palette = sns.color_palette()
 deep_palette = sns.color_palette("deep")
 
 TRIAGE_MAPPING = {
@@ -16,9 +17,20 @@ TRIAGE_MAPPING = {
 
 COLOR_MAPPING_DEEP = {
     "A": deep_palette[3],
-    "U": deep_palette[1],
+    "H": deep_palette[1],
     "V1": deep_palette[8],
     "V2": deep_palette[2]
 }
 
 COLOR_MAPPING_LIGHT = [plt.cm.colors.to_rgba(color, alpha=0.65) for color in COLOR_MAPPING_DEEP.values()]
+
+COLOR_MAPPING_NORMAL = {
+    "A": normal_palette[0],
+    "H": normal_palette[1],
+    "V1": normal_palette[2],
+    "V2": normal_palette[3]
+}
+
+COLOR_MAPPING_NORMAL_LIGHT = [plt.cm.colors.to_rgba(color, alpha=0.65) for color in COLOR_MAPPING_NORMAL.values()]
+
+
