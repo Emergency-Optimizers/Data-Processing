484fd13a6c0d41e9bfda6833a398e7a88cff7ca5
sindre0830 <sindre0830@gmail.com>
2024-06-09 13:37:26 +0200

    Add code documentation to main.py

--- source/main.py
+++ source/main.py
@@ -4,9 +4,11 @@ import pathing
 
 def main():
     """Main program."""
+    # runs data processing pipeline
     data_preprocessor = datahandler.DataPreprocessorOUS_V2()
     data_preprocessor.execute()
 
+    # builds OD cost matrix
     od = pathing.OriginDestination(
         dataset_id="oslo",
         utm_epsg=f"EPSG:326{33}"
